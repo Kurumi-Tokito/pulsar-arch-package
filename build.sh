4b76072fda6fb904cc328ecb15a7e7bfb3797628
Ivy-Tokito <93600306+Ivy-Tokito@users.noreply.github.com>
2024-06-09 17:07:34 +0530

    pulsar:Build With Workflows

--- build.sh
+++ build.sh
@@ -7,11 +7,17 @@ buildir="$(pwd)/build"
 pkgdir="$buildir/pkg"
 outdir="$buildir/out"
 
-export PKGVER="1.116.0"
+export PKGVER="1.117.0"
 
 echo "Install & upgrading Build Packages"
-sudo pacman -Sy  --noconfirm --needed base-devel gcc12 libxkbfile libsecret libx11 libxcrypt-compat jq git wget python-setuptools
+sudo pacman -Syuu --noconfirm --needed base-devel libxkbfile libsecret libx11 libxcrypt-compat jq git wget python-setuptools
 
+# get gcc12 from ArchLinux Archives
+wget -q https://archive.archlinux.org/packages/g/gcc12-libs/gcc12-libs-12.2.1-1-x86_64.pkg.tar.zst
+wget -q https://archive.archlinux.org/packages/g/gcc12/gcc12-12.2.1-1-x86_64.pkg.tar.zst
+sudo pacman -U gcc12-libs-12.2.1-1-x86_64.pkg.tar.zst --noconfirm
+sudo pacman -U gcc12-12.2.1-1-x86_64.pkg.tar.zst --noconfirm
+sudo pacman -Syuu --noconfirm --needed
 
 mkdir -p "$buildir"
 cd "$buildir" && echo "Entering BUILD DIR:$(pwd)" || exit 1
@@ -19,7 +25,7 @@ cd "$buildir" && echo "Entering BUILD DIR:$(pwd)" || exit 1
 # Clone Source
 git clone https://github.com/pulsar-edit/pulsar.git
 cd pulsar && echo "Entering pulsar source:$(pwd)" || exit 1
-git checkout "tags/v$pkgver"
+git checkout "tags/v$PKGVER"
 git log --oneline -5 && sleep 5 #show last 5 commits
 git submodule init && git submodule update
 sed -i -e "s/[0-9]*-dev/`date -u +%Y%m%d%H`/g" package.json
@@ -28,7 +34,7 @@ cd "$buildir/pulsar" && echo "Entering pulsar source:$(pwd)" || exit 1
 # instal nvm node manager
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. $NVM_DIR/nvm.sh
+[ -s "$NVM_DIR/nvm.sh" ] && source $NVM_DIR/nvm.sh
 
 # Install & Update node
 nvm install
@@ -41,8 +47,8 @@ export CC=gcc-12
 export CXX=g++-12
 
 # Optimize flags
-export CFLAGS=" -O3 -flto=auto -fuse-linker-plugin -mtune=generic -march=x86-64"
-export CXXFLAGS="-O3 -flto=auto -fuse-linker-plugin -mtune=generic -march=x86-64"
+export CFLAGS=" -O3 -flto=auto -fuse-linker-plugin -mtune=generic -march=x86-64-v3"
+export CXXFLAGS="-O3 -flto=auto -fuse-linker-plugin -mtune=generic -march=x86-64-v3"
 export LDFLAGS+=" -Wl,--no-keep-memory"
 
 cd "$buildir/pulsar" && echo "Entering pulsar source:$(pwd)" || exit 1
@@ -63,7 +69,10 @@ cp "$dir/PKGBUILD" "$pkgdir"
 
 cd "$pkgdir" && echo "Entering PKG-DIR: $(pwd)" || exit 1
 makepkg -CL
-cp "pulsar-$PKGVER-0-x86_64.pkg.tar.zst" "$outdir"
+cp "pulsar-$PKGVER-0-x86_64-v3.pkg.tar.zst" "$outdir"
+
+mkdir -p /build/packages
+find "/home/user/build" -type f -name "pulsar*.pkg*" -exec cp -v {} "/build/packages" \;
 
 echo "Packaging Completed"
 echo "Package: $(ls $outdir/*.zst)"
